f2b50464e18287af0c88fa11195e1450d9c26c3e
Amjad mehmood <73718759+amjad-awan@users.noreply.github.com>
2023-09-24 01:15:08 +0500

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const colors = require("colors");
 exports.connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL, {
-      dbName: "cobblar-and-kolachi", // Specify the database name here
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
